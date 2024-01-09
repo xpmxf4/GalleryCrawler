c4002dccc10e3d0d71e99b02c59b3a0d79646915
dev-chpark <xpmxf5@gmail.com>
2024-01-09 18:22:36 +0900

    🚦 test: TimerTask 작동 테스트

---
+++ src/main/java/org/example/FiveSecondTimer.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class FiveSecondTimer {
+
+    public static void main(String[] args) {
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                // 이 부분에 반복 실행하고 싶은 코드를 작성합니다.
+                System.out.println("타이머 작동 중: 5초마다 메시지 출력");
+            }
+        };
+
+        // 타이머를 5초(5000밀리초)마다 실행합니다.
+        timer.scheduleAtFixedRate(task, 0, 5000);
+    }
+}

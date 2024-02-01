7fd1554afe472eedbdf0af4563a4055be40c5639
dev-chpark <xpmxf5@gmail.com>
2024-02-01 11:06:59 +0900

    ⚙️ chore: 미사용 import 문 삭제

--- src/main/java/org/example/TotalViewCountCrawler.java
+++ src/main/java/org/example/TotalViewCountCrawler.java
@@ -6,7 +6,6 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.text.SimpleDateFormat;
 import java.util.Scanner;
 
 public class TotalViewCountCrawler {
